f843355709b2dcb4c35cca08c0738eeef7637c99
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2022-07-17 18:48:33 -0400

    js updated

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const drawButton = document.querySelector("#draw");
 const resetButton = document.querySelector("#reset");
-//const candidate = document.getElementById("name").value.split("\n");
-const candidate = ["a", "b", "c"];
-const randWinner = candidate[Math.floor(Math.random() * candidate.length)];
 
-console.log(randWinner);
+function randomSelect() {
+  const winnerEl = document.getElementById("winner");
+  winnerEl.innerHTML = "";
+  const candidates = document.getElementById("name").value;
+  if (candidates) {
+    const names = candidates.split(/\n/);
+    if (names.length > 0) {
+      const max = names.length - 1;
+      const winnerIndex = getRandomInt(0, max);
+      winnerEl.innerHTML = names[winnerIndex];
+    }
+  }
+}
 
-const winner = document.createElement("p");
-winner.textContent = randWinner;
-document.body.appendChild(winner);
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-drawButton.addEventListener("click", function () {
-  return winner;
-});
-
-console.log(winner);
+drawButton.addEventListener("click", randomSelect);
